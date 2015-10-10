872c989bd8264b7c790eb1379ffdf92600a15e10
theCompanyDream <tbrantleyii@gmail.com>
2015-10-10 15:57:11 -0400

    - Added Bootstrap LIbrary, routing to works correctly and moved some files around,

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nolongerAverage.net
 This project is for my new up and coming website that I'm publishing
+
+TODO: add testing for angular 
--- public/javascripts/clientAPi.js
+++ public/javascripts/clientAPi.js
@@ -1,13 +1,43 @@
 var homeApp = angular.module('resumeApp', ['ngRoute', 'experienceController']);
 
-homeApp.config(['$routeProvider',
-	function($routeProvider) {
+homeApp.config(['$routeProvider', "$locationProvider",
+	function($routeProvider, $locationProvider) {
 		$routeProvider
-			.when('/', {
-				templateUrl: 'content/partials/experience.html',
+			.when('/story', {
+				templateUrl: 'content/partials/story/experience.html',
 				controller: 'experience'
 			})
-			.otherwise({
-				redirectTo: '/'
+			.when('/events', {
+				templateUrl: 'content/partials/home.html',
+				controller: 'homePage'
+			})
+			.when('/api', {
+				templateUrl: 'content/partials/ignore.html',
+				controller: 'homePage'
+			})
+			.when('/contact', {
+				templateUrl: 'content/partials/home.html',
+				controller: 'homePage'
+			})
+			.when('/aboutme', {
+				templateUrl: 'content/partials/home.html',
+				controller: 'homePage'
+			})
+			.when('/', {
+				templateUrl: 'content/partials/home.html',
+				controller: 'homePage'
 			});
+			
+		$locationProvider.html5Mode(true);
+}]);
+
+homeApp.controller("homePage", ["$scope", function($scope) {
+	$scope.title = "Timothy Brantley super awesome website :)";
+	$scope.menus = [
+	{name:"Home", location: "/"}
+	,{name: "About Me", location: "aboutme"}
+	,{name:"Events", location: "events"}
+	,{name:"API", location: "api"}
+	,{name:"Story", location: "story"}
+	,{name: "Contact Me", location: "contact"} ];
 }]);
