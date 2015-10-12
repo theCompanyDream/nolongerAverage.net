db767810c9be40ee69bf963202ca7f8596bd7520
theCompanyDream <tbrantleyii@gmail.com>
2015-10-11 22:20:51 -0400

    This took a lot longer than I'm willing to admit >.>

--- public/javascripts/controllers/aboutMe.js
+++ public/javascripts/controllers/aboutMe.js
@@ -1,19 +1,32 @@
 var homePage = angular.module("baseApp");
 
+homePage.service('userInfoService',["$http", function($http){
+	this.GET = function(data) {
+		$http.get({
+			url: 'http://localhost:7000/content/api/aboutme.json'
+		}).success(function(json) {
+			console.log(json);
+			data(json);
+		}).error(function (err){
+			console.log(err);
+		});
+	}
+}]);
+
 homePage.controller("aboutme", ["$scope", "$http",
 	function($scope, $http){
-		$scope.data;
-		$http.get({
-			url: '/content/api/aboutme.json'
-		}).then(function success(response) {
-			data = response;
+		$http.get("/content/api/aboutme.json", []
+		).success(function(json) {
+			console.log(json);
+			$scope.data = json;
+		}).error(function (err){
+			console.log(err);
 		});
+}]);
 
-	}]);
-
-homePage.controller("contactUs", ["$scope", "$http",
+homePage.controller("contactUs", ["$scope",
 	function($scope, $http) {
 		$scope.facebook = "https://www.facebook.com/tbrantleyII";
 		$scope.lolking = "http://www.lolking.net/summoner/na/21692904";
 		$scope.email = "brantleyiit@gmail.com";
-	}]);
+}]);
