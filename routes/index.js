bf8a613f4091f89f49f96e1a8b0ec0ba2a79dc10
companydream <tbrantleyii@gmail.com>
2015-11-08 23:36:28 -0500

    Got the routes working correctly

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-function getIndex(req, res, next) {
+function getIndex(req, res) {
   res.render('index');
   next();
 }
 
-function getIndexId(req, res, next, id)
-{
-    res.render('index');
-    next();
-}
 
-function param(req, res, next, id) {
-  next();
-}
 
 /* GET home page. the only page*/
-router.param('id', param);
 router.all('/',     getIndex);
-router.all('/about/:Id', getIndexId);
+router.all('/aboutme/:Id', getIndex);
 
 module.exports = router;
